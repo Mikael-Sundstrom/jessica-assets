ef2f78eaa92e6affc7c2d17b21e79ce20d30eba8
Mikael Sundström <mikaelsundstrom@outlook.com>
2025-09-07 21:14:46 +0200

    Update

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	production: false,
+	firebase: {
+		apiKey: 'AIzaSyCggeMH0sML-h1j2uoKVP5KKTQKuAjNWmo',
+		authDomain: 'jessica-assets.firebaseapp.com',
+		projectId: 'jessica-assets',
+		storageBucket: 'jessica-assets.appspot.com',
+		messagingSenderId: '409194807563',
+		appId: '1:409194807563:web:xxxxxxx',
+	},
+}
